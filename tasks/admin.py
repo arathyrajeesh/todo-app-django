780503c74eeb273c82c723baaed319be293e1582
Arathy <arathyrajeesh2@gmail.com>
2025-09-24 19:59:05 +0530

    add superuser

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task  
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'created_at', 'complete_task')
+    list_filter = ('complete_task',)
+    search_fields = ('title', 'description')
