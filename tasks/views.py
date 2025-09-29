d62790795a57c4add17ac09f06bf0cc56d8cf47a
Arathy <arathyrajeesh2@gmail.com>
2025-09-29 09:55:30 +0530

    theme add

--- tasks/views.py
+++ tasks/views.py
@@ -59,10 +59,10 @@ def update_task(request, task_id):
 
 def task_list(request):
     tasks = Task.objects.all().order_by('-created_at')
-    pending_count = Task.objects.filter(complete_task=False).count()
+    pending_count = Task.objects.all().filter(complete_task=False).count()
     today=date.today()
     return render(request, 'task_list.html', {'tasks': tasks,
-                                                'false':pending_count,
+                                                'pending_count':pending_count,
                                                 'today':today})
 
 
